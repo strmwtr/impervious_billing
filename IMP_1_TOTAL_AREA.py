6d51aa970e52396c920ec1ecf3af2a13babccd75
Bob Brown <rb3@tutanota.com>
2017-09-28 11:45:12 -0400

    Changed add fields to a loop

--- IMP_1_TOTAL_AREA.py
+++ IMP_1_TOTAL_AREA.py
@@ -16,25 +16,24 @@ arcpy.env.overwriteOutput = True
 sde = r'Database Connections\Connection to GISPRDDB direct connect.sde'
 
 # Local variables:
-cvgis_CITY_parcel_area = sde + "\\cvgis.CITY.Cadastre\\cvgis.CITY.parcel_area"
-#cvgis_CITY_slab_area = sde + "\\cvgis.CITY.Buildings\\cvgis.CITY.slab_area"
-#cvgis_CITY_structure_existing_area = sde + "\\cvgis.CITY.Buildings\\cvgis.CITY.structure_existing_area"
-#cvgis_CITY_miscellaneous_building_area = sde + "\\cvgis.CITY.Buildings\\cvgis.CITY.miscellaneous_building_area"
-#cvgis_CITY_pedestrian_walkway_area = sde + "\\cvgis.CITY.Transportation_Other\\cvgis.CITY.pedestrian_walkway_area"
-#cvgis_CITY_road_area = sde + "\\cvgis.CITY.Transportation_Road\\cvgis.CITY.road_area_master"
-#cvgis_CITY_vehicle_driveway_area= sde + "\\cvgis.CITY.Transportation_Other\\cvgis.CITY.vehicle_driveway_area"
-#cvgis_CITY_vehicle_parking_area = sde + "\\cvgis.CITY.Transportation_Other\\cvgis.CITY.vehicle_parking_area"
+cvgis_CITY_parcel_area = sde + '\\cvgis.CITY.Cadastre\\cvgis.CITY.parcel_area'
+#cvgis_CITY_slab_area = sde + '\\cvgis.CITY.Buildings\\cvgis.CITY.slab_area'
+#cvgis_CITY_structure_existing_area = sde + '\\cvgis.CITY.Buildings\\cvgis.CITY.structure_existing_area'
+#cvgis_CITY_miscellaneous_building_area = sde + '\\cvgis.CITY.Buildings\\cvgis.CITY.miscellaneous_building_area'
+#cvgis_CITY_pedestrian_walkway_area = sde + '\\cvgis.CITY.Transportation_Other\\cvgis.CITY.pedestrian_walkway_area'
+#cvgis_CITY_road_area = sde + '\\cvgis.CITY.Transportation_Road\\cvgis.CITY.road_area_master'
+#cvgis_CITY_vehicle_driveway_area= sde + '\\cvgis.CITY.Transportation_Other\\cvgis.CITY.vehicle_driveway_area'
+#cvgis_CITY_vehicle_parking_area = sde + '\\cvgis.CITY.Transportation_Other\\cvgis.CITY.vehicle_parking_area'
 cvgis_CITY_parcel_point = sde + r'\cvgis.CITY.Cadastre\cvgis.CITY.parcel_point'
-#cvgis_CITY_pedestrian_sidewalk_area = sde + "\\cvgis.CITY.Transportation_Other\\cvgis.CITY.pedestrian_sidewalk_area"
-#cvgis_CITY_railroad_area = sde + "\\cvgis.CITY.Transportation_Railroad\\cvgis.CITY.railroad_area"
+#cvgis_CITY_pedestrian_sidewalk_area = sde + '\\cvgis.CITY.Transportation_Other\\cvgis.CITY.pedestrian_sidewalk_area'
+#cvgis_CITY_railroad_area = sde + '\\cvgis.CITY.Transportation_Railroad\\cvgis.CITY.railroad_area'
 
-intersect1 = gdb + r"\intersect1"
-dissolve1 = gdb + r"\dissolve1"
-all_imp = gdb + r"\all_imp"
-union1 = gdb + r"\union1"
-parcel_point_copy = gdb + r"\parcel_point"
-FINAL_IMP_POINTS = gdb + r"\FINAL_IMP_POINTS"
-
+intersect1 = gdb + r'\intersect1'
+dissolve1 = gdb + r'\dissolve1'
+all_imp = gdb + r'\all_imp'
+union1 = gdb + r'\union1'
+parcel_point_copy = gdb + r'\parcel_point'
+FINAL_IMP_POINTS = gdb + r'\FINAL_IMP_POINTS'
 
 #Copy Parcel points to gdb
 arcpy.CopyFeatures_management(cvgis_CITY_parcel_point, parcel_point_copy)
@@ -65,9 +64,6 @@ arcpy.Dissolve_management(intersect1, dissolve1, "GPIN", "", "MULTI_PART", "DISS
 
 # Process: Join Field
 arcpy.JoinField_management(parcel_point_copy, "PARCELSPOL", dissolve1, "GPIN", "")
-'''
-#-------------------------------------------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------------------------------------------
 
 # Process: Add Field (PIN)
 arcpy.AddField_management(parcel_point_copy, "PIN", "TEXT", "", "", "15", "", "NULLABLE", "NON_REQUIRED", "")
@@ -75,49 +71,22 @@ arcpy.AddField_management(parcel_point_copy, "PIN", "TEXT", "", "", "15", "", "N
 # Process: Calculate Field (PIN)
 arcpy.CalculateField_management(parcel_point_copy, "PIN", "[PROP_ID]", "VB", "")
 
+add_fields = ["STRUCTURE_AREA", "SLAB_AREA", "MISC_STRUCT_AREA", 
+  "PARKING_AREA", "DRIVEWAY_AREA", "SIDEWALK_AREA", "WALKWAY_AREA","ROAD_AREA",
+  "RAILROAD_AREA"]
+
+for field in add_fields:
+  arcpy.AddField_management(parcel_point_copy, field, "DOUBLE", "", 
+  "", "15", "", "NULLABLE", "NON_REQUIRED", "")
+
 # Process: Add Field (Total Area)
 arcpy.AddField_management(parcel_point_copy, "TOTAL_IMP_AREA", "DOUBLE", "", "", "", "", "NULLABLE", "NON_REQUIRED", "")
 
 # Process: Calculate Field (calculate Total IMP area)
 arcpy.CalculateField_management(parcel_point_copy, "TOTAL_IMP_AREA", "[Shape_Area]", "VB", "")
 
-# Process: Add Field (Structure_Area)
-arcpy.AddField_management(parcel_point_copy, "STRUCTURE_AREA", "DOUBLE", "", "", "15", "", "NULLABLE", "NON_REQUIRED", "")
-
-# Process: Add Field (Slab)
-arcpy.AddField_management(parcel_point_copy, "SLAB_AREA", "DOUBLE", "", "", "15", "", "NULLABLE", "NON_REQUIRED", "")
-
-# Process: Add Field (misc struct)
-arcpy.AddField_management(parcel_point_copy, "MISC_STRUCT_AREA", "DOUBLE", "", "", "15", "", "NULLABLE", "NON_REQUIRED", "")
-
-# Process: Add Field (vehicle parking)
-arcpy.AddField_management(parcel_point_copy, "PARKING_AREA", "DOUBLE", "", "", "15", "", "NULLABLE", "NON_REQUIRED", "")
-
-# Process: Add Field (vehicle driveway)
-arcpy.AddField_management(parcel_point_copy, "DRIVEWAY_AREA", "DOUBLE", "", "", "15", "", "NULLABLE", "NON_REQUIRED", "")
-
-# Process: Add Field (sidewalk)
-arcpy.AddField_management(parcel_point_copy, "SIDEWALK_AREA", "DOUBLE", "", "", "15", "", "NULLABLE", "NON_REQUIRED", "")
-
-# Process: Add Field (walkway)
-arcpy.AddField_management(parcel_point_copy, "WALKWAY_AREA", "DOUBLE", "", "", "15", "", "NULLABLE", "NON_REQUIRED", "")
-
-# Process: Add Field (road_area)
-arcpy.AddField_management(parcel_point_copy, "ROAD_AREA", "DOUBLE", "", "", "15", "", "NULLABLE", "NON_REQUIRED", "")
-
-# Process: Add Field (railroad_area)
-arcpy.AddField_management(parcel_point_copy, "RAILROAD_AREA", "DOUBLE", "", "", "15", "", "NULLABLE", "NON_REQUIRED", "")
-
-
-#-------------------------------------------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------------------------------------------
-#-------------------------------------------------------------------------------------------------------------------
-
 # Process: Remove Join
 #arcpy.RemoveJoin_management(parcel_point_copy) # might speed things up
 
 # Process: Copy out to final point featureclass
 arcpy.Copy_management(parcel_point_copy, FINAL_IMP_POINTS, "")
-
-'''
