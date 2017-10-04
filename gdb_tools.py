3feb5e9b1e3325b23a0b777cc7a00cf3be3818f3
Bob Brown <rb3@tutanota.com>
2017-10-04 15:20:28 -0400

    Added check_del to gdb_tools

--- gdb_tools.py
+++ gdb_tools.py
@@ -13,4 +13,17 @@ def wipe(geodatabase):
   for datasets in data_types:
     for data in datasets:
       arcpy.Delete_management(data)
-  
+
+def check_del(geodatabase, dataset):
+  arcpy.env.workspace = geodatabase
+
+  data_types = [
+    arcpy.ListDatasets(), 
+    arcpy.ListFeatureClasses(), 
+    arcpy.ListRasters(), 
+    arcpy.ListTables()
+    ]
+  for datasets in data_types:
+    if dataset in datasets:
+      arcpy.Delete_management(dataset)
+       
